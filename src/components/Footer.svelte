9a3110278e31af2c9f235536ed45cac052d2bb0e
peter <tttyyywa@gmail.com>
2023-12-16 09:55:28 +0200

    changed Footer color

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="py-3 pt-6 bg-[#d29d59] top-full w-full flex flex-col items-center">
+<footer class="py-3 pt-6 bg-[#38463a] text-white top-full w-full flex flex-col items-center">
 	<p>© peter</p>
 	<p>peter تم تصميمه بواسطة</p>
 </footer>
