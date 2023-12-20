977172535fb74f9f3f722cdcfe179a56f6f437c5
peter <tttyyywa@gmail.com>
2023-12-20 19:18:58 +0200

    added the AboutUs section

---
+++ src/components/AboutMini.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let aboutImg;
+	export let title;
+	export let paragraph;
+</script>
+
+<div class=" w-[80%] h-auto bg-gray-50 rounded-md shadow-lg text-center md:w-64 md:h-96">
+	<img src={aboutImg} alt="" class="rounded-t-md" />
+	<div class="p-4">
+		<h3 class="text-xl font-bold mb-5">{title}</h3>
+		<p class="">
+			{paragraph}
+		</p>
+	</div>
+</div>
--- src/components/AboutUs.svelte
+++ src/components/AboutUs.svelte
@@ -1,1 +1,44 @@
-<main></main>
+<script>
+	import about1 from './../assets/about-1.jpg';
+	import about2 from './../assets/about-2.jpg';
+	import about3 from './../assets/about-3.jpg';
+	import AboutMini from './AboutMini.svelte';
+</script>
+
+<section class="pb-20 bg-[#ddd3b8]">
+	<div class="px-8 text-center flex flex-col items-center">
+		<div class="my-5 mb-10 border-neutral-900 border-b-2 py-4">
+			<h2 class="text-4xl">نبذه عن المعهد</h2>
+		</div>
+		<p class="text-xl">
+			ومنذ نشاءت المعهد عام 1995 تعمل ادارتة على تكريس كافة الامكانيات الماديه والبشرية لتحقيق هذا
+			الهدف من خلال تكاتف كادفة الجهود سواء من قبل مجلس الادارة او اعضاء هيئية التدريس او اولياء
+			الامور وتوصلت إدارة المعهد إلى اختيار أربع تخصصات علمية رفيعة و هي : الدراسات السياحية (
+			الدراسات السياحة – إدارة الفنادق – الإرشاد السياحي) نظم المعلومات الإدارية- اللغات و الترجمة-
+			العلوم التجارية
+		</p>
+	</div>
+
+	<div class="flex flex-col flex-wrap mt-16 items-center gap-8 md:flex-row md:justify-center">
+		<AboutMini
+			title="قسم نظم المعلومات الادارية"
+			aboutImg={about1}
+			paragraph="معادلة الشهادات التي يمنحها القسم بدرجة البكالوريوس التي تمنحها الجامعات المصرية، و ذلك بموجب القرار الوزاري رقم 266"
+		/>
+		<AboutMini
+			title="العلوم التجارية"
+			aboutImg={about1}
+			paragraph="تمت معادلة الشهادات التى يمنحها - قسم العلوم التجارية (إدارة اعمال – محاسبة) قرار المعادلة رقم ١٢ طابع تنسيق ٣٢٨ /"
+		/>
+		<AboutMini
+			title="اللغات والترجمة"
+			aboutImg={about2}
+			paragraph="تمت معادلة الشهادات التى يمنحها - قسم اللغات والترجمة قرار المعادلة رقم ٨٠ – طابع تنسيق ٣٢٩/٨"
+		/>
+		<AboutMini
+			title="الدراسات السياحية"
+			aboutImg={about3}
+			paragraph="تمت معادلة الشهادات التى يمنحها قسم الدراسات السياحية (الدراسات السياحية – إدارة الفنادق- ارشاد سياحي ) قرار المعادلة رقم ٤٧ - طابع تنسيق رقم ٣٣٦"
+		/>
+	</div>
+</section>
