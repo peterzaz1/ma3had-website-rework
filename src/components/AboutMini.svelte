f0d538c013424b765e0ea1b4efffaa8326c643db
peter <tttyyywa@gmail.com>
2023-12-21 13:41:21 +0200

    AboutUs layout from flex to grid

--- src/components/AboutMini.svelte
+++ src/components/AboutMini.svelte
@@ -4,7 +4,9 @@
 	export let paragraph;
 </script>
 
-<div class=" w-[80%] h-auto bg-[#cea686] rounded-md shadow-lg text-center md:w-64 md:h-96">
+<div
+	class=" w-[80%] h-auto m-auto bg-[#cea686] rounded-md shadow-lg text-center md:m-0 md:w-80 md:h-96"
+>
 	<img src={aboutImg} alt="" class="rounded-t-md" />
 	<div class="p-4">
 		<h3 class="text-xl font-bold mb-5">{title}</h3>
